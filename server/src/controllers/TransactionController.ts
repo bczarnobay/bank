b4e4fb0c1d9367a4705a90cb5d8c26e36c4f808b
bczarnobay <35930918+bczarnobay@users.noreply.github.com>
2020-10-26 23:22:56 -0300

    MINOR: adding transaction body validations

--- server/src/controllers/TransactionController.ts
+++ server/src/controllers/TransactionController.ts
@@ -4,13 +4,23 @@ import TransactionService from '../services/TransactionService'
 class TransactionController {
   public async view (req: Request, res: Response): Promise<Response> {
     const { id } = req.params
-    return res.json(await TransactionService.getTransactionsByAccount(id))
+    const { limit, offset } = req.query
+
+    return res.json(await TransactionService.getTransactionsByAccount(id, Number(limit), Number(offset)))
   }
 
   public async create (req: Request, res: Response): Promise<Response> {
     const { id } = req.params
     const { amount, type, barcode } = req.body
     
+    if (amount < 0 ){
+      res.status(400).send({message: 'Amount must be bigger than 0 to perform the transaction'})
+    }
+
+    if (type === 'Payment' && barcode === null){
+      res.status(400).send({message: 'Barcode must be informed to perform the payment'})
+    }
+
     try {
       return res.json(await TransactionService.makeTransaction(id, amount, type, barcode))
     } catch (error) {
